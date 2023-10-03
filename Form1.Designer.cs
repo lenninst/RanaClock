ad65300a51204bb9b01aa121cb03588972ccb094
kanguro <leninguaminga@gmail.com>
2023-10-02 23:48:15 -0500

    rana upload first

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace RanaClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.txtMinute = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.txtMilliSecond = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnPause = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(51)))), ((int)(((byte)(71)))));
+            this.CloseButton.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(162)))), ((int)(((byte)(183)))));
+            this.CloseButton.Location = new System.Drawing.Point(533, 15);
+            this.CloseButton.Margin = new System.Windows.Forms.Padding(6);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(54, 29);
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.Text = "Cerrar";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            this.CloseButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtMinute
+            // 
+            this.txtMinute.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(72)))));
+            this.txtMinute.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMinute.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMinute.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(232)))), ((int)(((byte)(240)))));
+            this.txtMinute.Location = new System.Drawing.Point(126, 85);
+            this.txtMinute.Name = "txtMinute";
+            this.txtMinute.Size = new System.Drawing.Size(65, 46);
+            this.txtMinute.TabIndex = 1;
+            this.txtMinute.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(189)))), ((int)(((byte)(248)))));
+            this.btnPlay.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.ForeColor = System.Drawing.Color.Snow;
+            this.btnPlay.Location = new System.Drawing.Point(256, 157);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(6);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(68, 35);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(72)))));
+            this.txtSecond.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSecond.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtSecond.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSecond.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(232)))), ((int)(((byte)(240)))));
+            this.txtSecond.Location = new System.Drawing.Point(256, 85);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(65, 46);
+            this.txtSecond.TabIndex = 5;
+            this.txtSecond.TextChanged += new System.EventHandler(this.txtSecond_TextChanged_1);
+            // 
+            // txtMilliSecond
+            // 
+            this.txtMilliSecond.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(72)))));
+            this.txtMilliSecond.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMilliSecond.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtMilliSecond.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMilliSecond.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(232)))), ((int)(((byte)(240)))));
+            this.txtMilliSecond.Location = new System.Drawing.Point(378, 85);
+            this.txtMilliSecond.Name = "txtMilliSecond";
+            this.txtMilliSecond.Size = new System.Drawing.Size(70, 46);
+            this.txtMilliSecond.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(51)))), ((int)(((byte)(71)))));
+            this.btnPause.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPause.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(162)))), ((int)(((byte)(183)))));
+            this.btnPause.Location = new System.Drawing.Point(126, 161);
+            this.btnPause.Margin = new System.Windows.Forms.Padding(6);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(65, 29);
+            this.btnPause.TabIndex = 7;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(35)))), ((int)(((byte)(59)))));
+            this.ClientSize = new System.Drawing.Size(611, 250);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.txtMilliSecond);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.txtMinute);
+            this.Controls.Add(this.CloseButton);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.TextBox txtMinute;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.TextBox txtSecond;
+        private System.Windows.Forms.TextBox txtMilliSecond;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnPause;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RanaClock
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        private int clickCount = 0;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSecond_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            clickCount++;
+            
+
+            if (clickCount % 2 ==1 )
+            {
+                btnPlay.Text = "Stop";
+                stopwatch.Start();
+                timer1.Enabled = true;
+                
+            }
+            else
+            {
+                btnPlay.Text = "Play";
+                stopwatch.Stop();
+                txtMinute.Text = "00";
+                txtSecond.Text = "00";
+                txtMilliSecond.Text = "00";
+                timer1.Enabled = false;
+               
+
+            }
+
+
+        }
+    
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan timespan = new TimeSpan(0, 0, 0, 0, (int)stopwatch.ElapsedMilliseconds);
+            txtMinute.Text = timespan.Minutes.ToString().Length < 2
+                ? "0" + timespan.Minutes.ToString()
+                : timespan.Minutes.ToString();
+
+            txtSecond.Text = timespan.Seconds.ToString().Length < 2 
+                ? "0" + timespan.Seconds.ToString()
+                : timespan.Seconds.ToString();
+
+            txtMilliSecond.Text = timespan.Milliseconds.ToString().Length < 2 
+                ? "0" + timespan.Milliseconds.ToString()
+                : timespan.Milliseconds.ToString();
+
+        }
+
+        private void txtSecond_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {             
+                stopwatch.Stop();
+                btnPlay.Text = "Play";
+                    
+            
+        }
+    }
+}
